8a48de3207a891007b044ce73608842140ac78a2
taku <11235813213455k@gmail.com>
2020-05-08 12:13:21 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+import App from './component/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
